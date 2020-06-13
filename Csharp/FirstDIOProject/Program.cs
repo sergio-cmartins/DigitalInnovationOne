ce631aef6e5afed74e6a540a38a88f7e8a888f48
Sergio Martins <sergio.cmartins@gmail.com>
2020-06-13 10:19:43 -0300

    Nocoes Básicas C#, primeira aplicação criada pela CLI

---
+++ Csharp/FirstDIOProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FirstDIOProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NumeroDeVezes = 5;
+            for (int i=1; i<=NumeroDeVezes;i++){
+                System.Console.WriteLine($"Bem vindo ao curso de .net {i}");
+            }
+        }
+    }
+}
