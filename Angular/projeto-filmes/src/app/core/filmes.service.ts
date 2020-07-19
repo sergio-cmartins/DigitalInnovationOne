34036c676f899e6d3f0cfbb4211610f715684ebf
Sergio Martins <sergio.cmartins@gmail.com>
2020-07-19 17:13:08 -0300

    Inclusão da listagem e filtros dos filmes

--- Angular/projeto-filmes/src/app/core/filmes.service.ts
+++ Angular/projeto-filmes/src/app/core/filmes.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Filme } from '../shared/models/filme';
 
@@ -14,5 +14,21 @@ export class FilmesService {
 
   salvar(filme: Filme): Observable<Filme> {
     return this.http.post<Filme>(url, filme);
+  }
+
+  listar(pagina: number, qtdPagina: number, texto: string, genero: string): Observable<Filme[]> {
+    let  httpParams = new HttpParams();
+    httpParams = httpParams.set('_page', pagina.toString());
+    httpParams = httpParams.set('_limit', qtdPagina.toString());
+    httpParams = httpParams.set('_sort', 'id');
+    httpParams = httpParams.set('_order', 'desc');
+    if (texto) {
+      httpParams = httpParams.set('q', texto);
+    }
+    if (genero) {
+      httpParams = httpParams.set('genero', genero);
+    }
+
+    return this.http.get<Filme[]>(url, {params: httpParams});
   }
 }
--- Angular/projeto-filmes/src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ Angular/projeto-filmes/src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,4 +1,8 @@
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+
+import { FilmesService } from './../../core/filmes.service';
+import { Filme } from 'src/app/shared/models/filme';
 
 @Component({
   selector: 'dio-listagem-filmes',
@@ -7,13 +11,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListagemFilmesComponent implements OnInit {
 
-  constructor() { }
+  readonly qtdPagina = 4;
+  pagina = 0;
+  filmes: Filme[] = [];
+  filtrosListagem: FormGroup;
+  generos: Array<string>;
+  texto: string;
+  genero: string;
+
+
+  constructor(
+    private filmeService: FilmesService,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
 
+    this.filtrosListagem = this.fb.group({
+      texto: [''],
+      genero: ['']
+    });
+
+    this.generos = [
+      'Ação',
+      'Aventura',
+      'Ficção Científica',
+      'Romance',
+      'Terror',
+      'Comédia',
+      'Drama'
+    ];
+
+    this.filtrosListagem.get('texto').valueChanges.subscribe((val: string) => {
+      this.texto = val;
+      this.resetarConsulta();
+    });
+
+    this.filtrosListagem.get('genero').valueChanges.subscribe((val: string) => {
+      this.genero = val;
+      this.resetarConsulta();
+    });
+
+    this.listarFilmes();
   }
 
-  open() {
+  onScroll(): void {
+    this.listarFilmes();
+  }
+
+  private listarFilmes(): void {
+    this.pagina++;
+    this.filmeService.listar(this.pagina, this.qtdPagina, this.texto, this.genero)
+      .subscribe((filmes: Filme[]) => this.filmes.push(...filmes));
+  }
+
+  private resetarConsulta(): void {
+    this.pagina = 0;
+    this.filmes = [];
+    this.listarFilmes();
   }
 
 }
